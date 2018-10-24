fbfffcf0111ef94d910df8dac46ce8748085f8dc
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-10-24 10:14:30 +0200

    Improve code

--- beespeller.py
+++ beespeller.py
@@ -4,14 +4,17 @@ from sys import argv
 
 if __name__ == '__main__':
     mandatory_letter = argv[1].lower()
-    allowed_letters = set(argv[2].lower()) | set(mandatory_letter)
-
-    with open('words_alpha.txt', 'r') as words:
-        for line in words:
-            word = line.rstrip('\n')
-            
-            if len(word) >= 4:
-                candidate_letters = set(word.lower())
-                if (mandatory_letter in candidate_letters and
-                    candidate_letters.issubset(allowed_letters)):
-                    print(word)
+    allowed_letters = frozenset(argv[2].lower()) | frozenset(mandatory_letter)
+    
+    def is_valid(word: str):
+        if len(word) >= 4:
+            candidate_letters = frozenset(word.lower())
+            return (mandatory_letter in candidate_letters and
+                    candidate_letters.issubset(allowed_letters))
+        else:
+            return False
+    
+    with open('words_alpha.txt', 'r') as wordfile:
+        for word in (line.rstrip('\n') for line in wordfile):
+            if is_valid(word):
+                print(word)
